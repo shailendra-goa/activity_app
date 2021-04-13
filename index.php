863d359640a225974a7f7861a0fd6460e5bfcccc
Shailendra Malondkar <shailendra_m2@yahoo.com>
2021-04-13 15:53:50 +0530

    added contact page

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
         <a class="nav-link" href="#">Activity</a>
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Contact</a>
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </div>
     </div>
