cf1db285fd9b4319e4e68d0380dda371161f8d02
Shailendra Malondkar <shailendra_m2@yahoo.com>
2021-04-14 12:24:35 +0530

    after adding add functionality

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "activity_app";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $sql = "INSERT INTO activity (name, description, category, activity_date)
+  VALUES ('".$_POST['title']."', '".$_POST['description']."', '".$_POST['category']."','".$_POST['date']."')";
+  echo $sql;
+
+
+
+  if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    unset($_POST);
+    header("Location: ".$_SERVER[REQUEST_URI]);
+  exit;
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,13 +52,72 @@
     </div>
   </div>
 </nav>
-<div class="container">
+<div class="container-fluid">
+  <div class="row">&nbsp;</div>
   <div class="row">
-    <div class="col">col</div>
-  </div>
-  <div class="row">
-    <div class="col-8">col-8</div>
-    <div class="col-4">col-4</div>
+    <div class="col-4">
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <div class="col">
+          <div class="mb-3">
+            <label class="form-label">Activity Title</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Morning Ride">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Description</label>
+            <textarea class="form-control" name="description" rows="3"></textarea>
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Category</label>
+             <input type="text" class="form-control" id="category" name="category" placeholder="Cycling">
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Date</label>
+            <input type="date" class="form-control" id="date" name="date">
+          </div>
+          <div class="mb-3">
+            <button type="submit" class="btn btn-dark float-right">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-8">
+      <div class="container">
+        <div class="row" >
+          <div class="col-1 border bg-light"><b>Id</b></div>
+          <div class="col-3 border bg-light"><b>Title</b></div>
+          <div class="col-3 border bg-light"><b>Activity</b></div>
+          <div class="col-2 border bg-light"><b>Date</b></div>
+          <div class="col-3 border bg-light"><b>Actions</b></div>
+        </div>
+      </div>
+      <div class="container">
+<?php
+      $sql = "SELECT id, name, description, category, activity_date FROM activity order by id desc";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+?>
+        <div class="row" >
+          <div class="col-1 border bg-light"><?= $row["id"] ?></div>
+          <div class="col-3 border bg-light"><?= $row["description"] ?></div>
+          <div class="col-3 border bg-light"><?= $row["category"] ?></div>
+          <div class="col-2 border bg-light"><?= $row["activity_date"] ?></div>
+          <div class="col-3 border bg-light"><a href='update_product.php?id={$id}' class='btn btn-info'> Edit </a> <a delete-id='{$id}' class='btn btn-danger'>Delete</a></div>
+        </div>
+<?php
+        }
+      } else {
+?>
+        <div class="row" >
+            <div class="col alert alert-danger">No activities. Please add.</div>
+        </div>
+<?php 
+      }
+?>
+      </div>
+    </div>
   </div>
 </div>
 </body>
